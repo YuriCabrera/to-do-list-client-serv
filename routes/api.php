c58d03ef169a26ebf315a5c81a19141c8178e197
Yuri Cabrera <cabreray399@gmail.com>
2025-07-29 17:17:22 -0400

    Ajustado API

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,13 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TarefaController;
 
-Route::get('/tarefa', [TarefaController::class, 'index']);         // Listar todas
-Route::post('/tarefa', [TarefaController::class, 'store']);        // Criar nova
-Route::put('/tarefa/{id}', [TarefaController::class, 'update']);   // Atualizar
-Route::delete('/tarefa/{id}', [TarefaController::class, 'destroy']); // Excluir
+Route::get('/tarefa', [TarefaController::class, 'index']);      // listar todas
+
+Route::post('/tarefa', [TarefaController::class, 'store']);       // criar
+
+Route::put('/tarefa/{id}', [TarefaController::class, 'update']);   // atualizar
+
+Route::delete('/tarefa/{id}', [TarefaController::class, 'destroy']); // excluir
 
 
 ?>
